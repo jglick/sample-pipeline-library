d70cc49f0e4fabcfd337284337533e1986f0d20c
Jesse Glick <jglick@cloudbees.com>
2019-04-25 09:23:10 -0400

    class def

---
+++ src/com/github/jglick/sample/Err.groovy
@@ -0,0 +1,6 @@
+package com.github.jglick.sample
+class Err {
+    static void call(script) {
+        script.error 'oops'
+    }
+}
--- src/com/github/jglick/sample/err.groovy
+++
@@ -1,4 +0,0 @@
-package com.github.jglick.sample
-static void call(script) {
-    script.error 'oops'
-}
